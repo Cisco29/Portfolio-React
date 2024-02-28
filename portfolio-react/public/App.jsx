d499d5f6e27a5c92b584598fca199dd6af2b97ea
Cisco29 <castro.francisco@live.fr>
2024-02-28 21:24:00 +0100

    Mise en place NavBar

---
+++ portfolio-react/public/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../src/components/NavBar/NavBar";
+
+const App = () => {
+    return (
+        <>
+        <NavBar />
+        </>
+    );
+};
+
+export default App;
