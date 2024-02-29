0106dfeb7e4c4a24b5f7e80eedb54c5b614560cb
Cisco29 <castro.francisco@live.fr>
2024-02-29 12:23:09 +0100

    fin concernant le responsive de la navbar

--- portfolio-react/src/components/NavBar/NavBar.jsx
+++ portfolio-react/src/components/NavBar/NavBar.jsx
@@ -3,31 +3,40 @@ import './NavBar.css'
 
 
 export const NavBar = () => {
-  return (
-    <nav className="nav-wrapper">
-        <div className="nav-content">
-        <img className="logo" src="/assets/images/logo.jpg" alt="" width="200" height="100" />
-
-            <ul>
-                <li>
-                    <a className="menu-item"> Accueil</a>
-                </li>
-                <li>
-                    <a className="menu-item"> Skills</a>
-                </li>
-                <li>
-                    <a className="menu-item"> Expérience professionnelle</a>
-                </li>
-                <li>
-                    <a className="menu-item"> Accueil</a>
-                </li>
-
-                <button className='contact-btn' onClick={() => {}}>
-                    Hire Me
-                </button>
-            </ul>
-        </div>
-    </nav>
-  )
-}
+    return (
+      <nav className="nav-wrapper">
+          <div className="nav-content">
+              <img className="logo" src="/assets/images/logo.jpg" alt="" width="200" height="100" />
+  
+              <ul>
+                  <li>
+                      <a  className="menu-item"> Accueil</a>
+                  </li>
+                  <li>
+                      <a className="menu-item"> Skills</a>
+                  </li>
+                  <li>
+                      <a className="menu-item"> Expérience professionnelle</a>
+                  </li>
+                  <li>
+                      <a className="menu-item"> Accueil</a>
+                  </li>
+  
+                  <button className='contact-btn' onClick={() => {}}>
+                      Hire Me
+                  </button>
+              </ul>
+              <button className="menu-btn" onClick={() => {}}>
+                <span
+                className={"material-symbols-outlined"}
+                style={{ fontSize: "0.6rem"}}
+                >
+                menu
+                </span>
+              </button>
+          </div>
+      </nav>
+    )
+  }
+  
 export default NavBar
