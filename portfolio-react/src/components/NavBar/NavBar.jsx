16695992592087252362e6972747bc9d78ecac46
Cisco29 <castro.francisco@live.fr>
2024-02-29 10:11:50 +0100

    avancement css nav bar logo ect

--- portfolio-react/src/components/NavBar/NavBar.jsx
+++ portfolio-react/src/components/NavBar/NavBar.jsx
@@ -4,7 +4,30 @@ import './NavBar.css'
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className="nav-wrapper">
+        <div className="nav-content">
+        <img className="logo" src="/assets/images/logo.jpg" alt="" width="200" height="100" />
+
+            <ul>
+                <li>
+                    <a className="menu-item"> Accueil</a>
+                </li>
+                <li>
+                    <a className="menu-item"> Skills</a>
+                </li>
+                <li>
+                    <a className="menu-item"> Expérience professionnelle</a>
+                </li>
+                <li>
+                    <a className="menu-item"> Accueil</a>
+                </li>
+
+                <button className='contact-btn' onClick={() => {}}>
+                    Hire Me
+                </button>
+            </ul>
+        </div>
+    </nav>
   )
 }
 export default NavBar
